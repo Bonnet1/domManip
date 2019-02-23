d53157f082b1f784529f72fa161ab451eaf84505
Bonnet1 <c.mannion84@gmail.com>
2019-02-23 17:33:37 -0500

    Completed intro DOM manipulation exercises

--- object.js
+++ object.js
@@ -14,9 +14,19 @@ heading3.textContent = "I'm a blue h3!";
 
 container.appendChild(heading3);
 
+const heading1 = document.createElement('H1');
+heading1.classList.add('heading1');
+heading1.textContent = "I'm in a div";
+
+const para = document.createElement('p');
+para.classList.add('para');
+para.textContent = "ME TOO!"
+
 const moreStuff = document.createElement('div');
 moreStuff.classList.add('moreStuff');
 moreStuff.setAttribute('style', 'color: black; background: pink; border: thick solid black;');
-moreStuff.textContent = "I'm a blue h3!";
+
+moreStuff.appendChild(heading1);
+moreStuff.appendChild(para);
 
 container.appendChild(moreStuff);
